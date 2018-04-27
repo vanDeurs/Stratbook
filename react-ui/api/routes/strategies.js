43119c1c84122fa14d292780dbd1191ef659ced2
Alexander Wilson van Deurs <alexwvd@outlook.com>
2018-04-27 18:05:57 +0200

    plz work

--- react-ui/api/routes/strategies.js
+++ react-ui/api/routes/strategies.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const testData = require('../../src/data/testData.json');
 const router = express.Router()
+const path = require('path');
 
-router.get('/', (req, res, next) => {
-    res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.stringify(testData.user.strategies))
+// router.get('/', (req, res, next) => {
+//   res.setHeader('Content-Type', 'application/json');
+//   res.send(JSON.stringify(testData.user.strategies));
+//   // res.sendFile(path.resolve(__dirname, '../../build', 'index.html'));
+//   });
+
+router.get('/', (req, res) => {
+  // res.setHeader('Content-Type', 'application/json');
+  const strats = JSON.stringify(testData.user.strategies);
+  res.send(JSON.parse(strats));
   });
 
 module.exports = router;
