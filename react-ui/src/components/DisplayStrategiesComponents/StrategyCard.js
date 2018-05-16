56d1348be9e1efa6c1032e5519b1e118dc541251
Alexander Wilson van Deurs <alexwvd@outlook.com>
2018-05-16 16:27:22 +0200

    New StrategyCard design

--- react-ui/src/components/DisplayStrategiesComponents/StrategyCard.js
+++ react-ui/src/components/DisplayStrategiesComponents/StrategyCard.js
@@ -1,5 +1,7 @@
 import React, { Component }     from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
 
 import ReactDOM         from 'react-dom';
 import {App}            from '../../index';
@@ -29,14 +31,41 @@ export class StrategyCard extends Component {
 
         return(
             <div className="strategyCardContainer">
-                <h1>{mapName+ ' ' + strategyName}</h1>
-                <h2>Summary: {strategySummary}</h2>
-                <h2>Explanation: {strategyExplanation}</h2>
-                <h2>ID: {strategyId}</h2>
-                <h2>Type: {strategyType}</h2>
-                <h2>Created: {strategyCreated}</h2>
-                
+                <div className="cardTopWrapper">
+                    <div className="cardNameDiv"> 
+                        <h3 className="cardHeader">Map: </h3>
+                        <h3 className="cardData">{mapName}</h3>
+                    </div>
+                    <div className="cardNameDiv"> 
+                        <h3 className="cardHeader">Name: </h3>
+                        <h3 className="cardData">{strategyName}</h3>
+                    </div>
+                    <div className="cardTypeDiv"> 
+                        <h3 className="cardHeader">Type: </h3>
+                        <h3 className="cardData">{strategyType}</h3>
+                    </div>
+                </div>
+                <div className="cardBottomWrapper">
+                    <div className="cardSummaryDiv"> 
+                        <h3 className="cardSummaryHeader">Summary: </h3>
+                        <h3 className="cardSummaryText">{strategySummary}</h3>
+                    </div>
+                    <div className="cardCreatedDiv"> 
+                        <h3 className="cardCreatedHeader">Created: </h3>
+                        <h3 className="cardCreatedSubHeader"> {strategyCreated}</h3>
+                    </div>
+                </div>
             </div>
         );
     };
 };
+
+StrategyCard.propTypes = {
+    mapName: PropTypes.string.isRequired,
+    strategyName: PropTypes.string.isRequired,
+    strategySummary: PropTypes.string.isRequired,
+    strategyExplanation: PropTypes.string.isRequired,
+    strategyId: PropTypes.number.isRequired,
+    strategyType: PropTypes.string.isRequired,
+    strategyCreated: PropTypes.string.isRequired,
+  };
