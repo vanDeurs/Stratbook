48bee7297062d1a33a2fe31fbe39012d3446dc9c
Alexander Wilson van Deurs <alexwvd@outlook.com>
2018-05-31 22:35:37 +0200

    Re-structured files and so rewrote some stuff in the server

--- server/index.js
+++ server/index.js
@@ -2,29 +2,29 @@ const testData = require('../react-ui/src/data/testData.json');
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const sqlite3 = require('sqlite3');
 
+const {createStrategyTable} = require('../react-ui/api/sql');
+
+const loggin = morgan('dev');
 const app = express();
-
 const PORT = process.env.PORT || 5000;
 
 // Priority serve any static files.
 app.use(express.static(path.resolve(__dirname, '../react-ui/build')));
 app.use(bodyParser.json());
+app.use(morgan('dev'));
 
-
-// Strategies router
-const strategiesRouter = require('../react-ui/api/routes/strategies');
-app.use('/:map/strategies', strategiesRouter)
-
-// Setups router
-// const setupsRouter = require('../react-ui/api/routes/setups');
-// app.use('/:map/setups', setupsRouter)
+// Router
+const router = require('../react-ui/api/routes/index.js');
+app.use('/', router);
 
 // All remaining requests return the React app, so it can handle routing.
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, '../react-ui/build', 'index.html'));
 });
 
-app.listen(PORT,() => {
+app.listen(PORT, () => {
   console.error(`Listening on port ${PORT}`);
 });
