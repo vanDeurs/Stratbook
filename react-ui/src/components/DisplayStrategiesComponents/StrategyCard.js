54ba518c0e8067671678a9391c7aa957b811a907
Alexander Wilson van Deurs <alexwvd@outlook.com>
2018-05-31 12:26:49 +0200

    merge

--- react-ui/src/components/DisplayStrategiesComponents/StrategyCard.js
+++ react-ui/src/components/DisplayStrategiesComponents/StrategyCard.js
@@ -1,7 +1,6 @@
 import React, { Component }     from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import PropTypes from 'prop-types';
-import FontAwesome from 'react-fontawesome';
 
 
 import ReactDOM         from 'react-dom';
