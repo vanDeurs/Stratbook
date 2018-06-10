9511403462f80e485af1d52789f74dcc705b3bbd
Alexander Wilson van Deurs <alexwvd@outlook.com>
2018-06-10 21:26:24 +0200

    Made header component into a functional component

--- react-ui/src/components/Header.js
+++ react-ui/src/components/Header.js
@@ -1,37 +1,16 @@
-import React, { Component }     from 'react';
-import ReactDOM         from 'react-dom';
-import {App}            from '../index';
+import React from 'react';
 import '../styles/index.css';
-import {Link, NavLink} from 'react-router-dom';
+import {NavLink} from 'react-router-dom';
 
-export class Header extends Component {
-    constructor(props){
-        super(props)
-        this.state = {
-            currentMap: 'Current map'
-        }
-    }
-
-    render(){
-        return(
-            <nav>
-            <div className="navWide">
-                <div className="wideDiv">
-                    <NavLink to='/' activeClassName='hurray'>Home</NavLink>
-                    {/* <NavLink to='/' activeClassName='hurray'>React</NavLink> */}
-                    {/* <NavLink to='/react' activeClassName='hurray'>React</NavLink> */}
+export const Header = ({props}) => {
+    return(
+        <nav>
+            <div className="navWrapper">
+                <div className="navContainer">
+                    <NavLink to='/' className='navLink'>Home</NavLink>
                 </div>
             </div>
-            {/* <div className="navNarrow">
-                <i className="fa fa-bars fa-2x" onClick={this.burgerToggle}></i>
-                <div className="narrowLinks">
-                    <a href="#">Link 1</a>
-                    <a href="#">Link 2</a>
-                    <a href="#">Link 3</a>
-                </div>
-            </div> */}
         </nav>
-        )
-    }
+    )
 }
     
